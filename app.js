d07dcba7fa43ddeccfbf2c27703b76f0c411b1f2
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-08-18 20:41:16 +0530

    Fix server path errors (#3)

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(config.serverRoot, express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: config.sessionSecret,
@@ -24,7 +25,8 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }))
-app.use('/', indexRouter);
+
+app.use(config.serverRoot, indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
